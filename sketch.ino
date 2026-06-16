aeecc1d4b932b00e3df2259f86ec8441f21c32d0
kpnikam-08 <kp.nikam08@gmail.com>
2026-06-16 15:37:39 +0530

    Day 1: Hello Counter using Serial Monitor

--- sketch.ino
+++ sketch.ino
@@ -1,17 +1,21 @@
-int LED1=26;
+int LED1 = 26;
+int count = 1;
+
 void setup() {
-// put your setup code here, to run once:
- Serial.begin(115200);
- Serial.println("Hello, ESP32!"); 
- pinMode (LED1, OUTPUT);
+  Serial.begin(115200);
+  pinMode(LED1, OUTPUT);
 }
+
 void loop() {
-digitalWrite(LED1, HIGH);
-Serial.println("ON");
-delay(1000);
-digitalWrite(LED1, LOW);
-Serial.println("OFF");
-delay (1000);
-// put your main code here, to run repeatedly:
-// this speeds up the simulation
+  digitalWrite(LED1, HIGH);
+  Serial.print("Hello ");
+  Serial.println(count);
+
+  delay(1000);
+
+  digitalWrite(LED1, LOW);
+
+  delay(1000);
+
+  count++;
 }
